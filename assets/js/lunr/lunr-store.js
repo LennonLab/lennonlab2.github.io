98dd141d2bc390a30e6925fe5e6c155cdf574aa6
jaytlennon <jaytlennon@users.noreply.github.com>
2025-04-16 06:01:06 +0000

    deploy: b347868f97ecfed1a8438c3c97f193be9e762079

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,55 @@
+var store = [{
+        "title": "404",
+        "excerpt":"     404     Page not found :(    The requested page could not be found.   ","categories": [],
+        "tags": [],
+        "url": "/404.html",
+        "teaser": null
+      },{
+        "title": "CV",
+        "excerpt":"Download CV  ","categories": [],
+        "tags": [],
+        "url": "/cv/",
+        "teaser": null
+      },{
+        "title": "Home",
+        "excerpt":"Microorganisms are the most abundant and diverse life forms on Earth. They have fast reproductive rates and evolve rapidly to changes in their environment. Microbes also carry out processes that are critical for the stability of engineered, environmental, and host-associated ecosystems. Our group is in the Department of Biology at...","categories": [],
+        "tags": [],
+        "url": "/",
+        "teaser": null
+      },{
+        "title": "Lab Info",
+        "excerpt":"  Contact Info   Lennon Lab   Department of Biology   Indiana University   1001 East 3rd Street   Bloomington, IN 47401       ","categories": [],
+        "tags": [],
+        "url": "/info/",
+        "teaser": null
+      },{
+        "title": "Opportunities",
+        "excerpt":"Science is most successful when ideas and knowledge from a variety of perspectives and experiences are openly and respectfully exchanged. Together, our team works to cultivate a safe environment where everyone is treated with respect regardless of race, ethnicity, religion, gender identification, sexual orientation, age, disability status, or economic background....","categories": [],
+        "tags": [],
+        "url": "/opportunities/",
+        "teaser": null
+      },{
+        "title": "People",
+        "excerpt":"Jay T. Lennon Principal Investigator Research Interests: Ecology, evolution, and biodiversity of microbial systems Google Scholar ORCiD CV Jipeng Luo Postdoctoral researcher Research Interests: Ecology and evolution of microbial communities, plant-microbiome interactions, evolutionary genomics, soil remediation Emma Bueren Postdoctoral researcher Research Interests: Bactere-phage evolutionary ecology, nested symbioses, host microbiomes Sungyun...","categories": [],
+        "tags": [],
+        "url": "/people/",
+        "teaser": null
+      },{
+        "title": "Publications",
+        "excerpt":"Lennon JT, Rappuoli R, Bloom DE, Brooks CG, Egamberdieva D, Lawley TD, Morhard R, Mukhopadhyay A, Nguyen N, Peixoto RS, Silver PA, Stein LY (2025) Microbial solutions for climate change require global partnership. mBio 16: 10.1128/mbio.00778-25 (pdf) Rappuoli R, Nguyen N, Bloom DE, Brooks CG, Egamberdieva D, Lawley TD, Morhard...","categories": [],
+        "tags": [],
+        "url": "/publications/",
+        "teaser": null
+      },{
+        "title": "Research",
+        "excerpt":"From test tubes to the globe, we study the ecology and evolution of microbial communities. We are interested in the factors that generate and maintain microbial biodiversity. In turn, we seek to understand the implications of microbial diversity for ecosystem functioning. To address these questions, our team uses a combination...","categories": [],
+        "tags": [],
+        "url": "/research/",
+        "teaser": null
+      },{
+        "title": "Teaching",
+        "excerpt":"Quantitative Biodiversity (QB) In this graduate-level course we focus on theory, patterns, metrics, and tools used to study biodiversity. We calculate diversity metrics, generate and quantify diversity relationships, visualize multivariate data, and conduct phylogenetic tests. We use modern statistical computing and graphics environments (i.e., R), as well as version control...","categories": [],
+        "tags": [],
+        "url": "/teaching/",
+        "teaser": null
+      },]
